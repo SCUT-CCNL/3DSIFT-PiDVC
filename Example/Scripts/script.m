e0c0f4e4d8435371080d80de7ad2ed319e2f8a99
unknown <metalyounglot@gmail.com>
2020-05-30 14:48:22 +0800

    update script

--- Example/Scripts/script.m
+++ Example/Scripts/script.m
@@ -1,16 +1,30 @@
 
-m_filepath = 'D:\SIFT_EXPERI_LOG\Hole_intersect\special\PiDVC\';
-m_filepath = [m_filepath, '624_500-Ori300Margin162_162_100-m12_std8_r8-intersect_1875-split20-rot30-cos-GN_Strategy2_CPU_PiSIFT_N16_Ransac30_3.2_Sub33_2020-03-29-07-01.txt']
+m_filepath_Dir = 'D:\Results\';
+m_filepath = [m_filepath_Dir, 'Simulated_deformed_large_inhomogeneous.nii_PiDVC_CPU_PiSIFT_N16_Ransac30_3.2_Sub33_2020-05-30-13-53.txt']
 
 %the start line number of POI results in the text file
-startLine = 48;
+startLine = 45;
 %read the text
-[x_pos, y_pos, z_pos,zncc, u, v, w,u0, v0, w0, ux, uy, uz, vx, vy, vz, wx, wy, wz,  iteration, range,candidate_num,final_num,edge,ROI_flag,val,distance,mode,converge, radius] = textread(m_filepath, '%d%d%d%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%d%d%d%d%d%f%f%f%d%d%f','delimiter',',','headerlines', startLine); 
+[x_pos, y_pos, z_pos,zncc, u, v, w,u0, v0, w0, ux, uy, uz, vx, vy, vz, wx, wy, wz,  iteration,ROI_flag, converge, strategy,candidate_num,final_num] = textread(m_filepath, '%d%d%d%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%d%d%d%d%d%d','delimiter',',','headerlines', startLine); 
 
 %plot the results
-%plot U-displacement
+%plot U-displacement distribution
 figure('NumberTitle', 'off', 'Name', 'U-displacement');
 scatter3(x_pos, y_pos, z_pos, 75, u, '.');
+xlabel('x'); ylabel('y'); zlabel('z');
+xlim([150, 475]); ylim([150, 475]); zlim([90,410]);
+colorbar; 
+hold on;
+ax = gca;
+ax.BoxStyle = 'full';
+box on
+set(gcf,'color',[1,1,1])
+set(gca, 'FontSize', 18)
+set(gca, 'FontName', 'Times New Roman')
+
+%plot W-displacement distribution
+figure('NumberTitle', 'off', 'Name', 'W-displacement');
+scatter3(x_pos, y_pos, z_pos, 75, w, '.');
 xlabel('x'); ylabel('y'); zlabel('z');
 xlim([150, 475]); ylim([150, 475]); zlim([90,410]);
 colorbar; 
